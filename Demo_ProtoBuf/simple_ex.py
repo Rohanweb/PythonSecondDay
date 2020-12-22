c73cd4e42b596807aa378604a2d914b21d816956
amitsharmadev <amit.sharma.dev@gmail.com>
2020-12-22 17:38:57 +0530

    Final1

--- Demo_ProtoBuf/simple_ex.py
+++ Demo_ProtoBuf/simple_ex.py
@@ -1,20 +1,20 @@
 import simple_pb2 as simple_pb2
 
-simple_message = simple_pb2.SimpleMessage()
-simple_message.id = 123
-simple_message.is_simple = True
-simple_message.name = "This is a simple Message"
-sample_list = simple_message.sample_list
-sample_list.append(3)
-sample_list.append(4)
-sample_list.append(5)
+# simple_message = simple_pb2.SimpleMessage()
+# simple_message.id = 123900099
+# simple_message.is_simple = True
+# simple_message.name = "This is a simple Message"
+# sample_list = simple_message.sample_list
+# sample_list.append(3)
+# sample_list.append(4)
+# sample_list.append(5)
 
-print(simple_message)
+# print(simple_message)
 
-with open("simple.bin", "wb") as f:
-    print("write as binary")
-    bytesAsString = simple_message.SerializeToString()
-    f.write(bytesAsString)
+# with open("simple.bin", "wb") as f:
+#     print("write as binary")
+#     bytesAsString = simple_message.SerializeToString()
+#     f.write(bytesAsString)
 
 
 with open("simple.bin", "rb") as f:
@@ -24,6 +24,8 @@ with open("simple.bin", "rb") as f:
 
 print(simple_message_read)
 
+print(simple_message_read)
 
-print("Is Simple?: " + str(simple_message.is_simple))
-print("Id?: " + str(simple_message.id))
+
+# print("Is Simple?: " + str(simple_message.is_simple))
+# print("Id?: " + str(simple_message.id))
